ea6efed3ab2855b18758ba1b85d380dc567ea547
Gilbert <ldes89150@gmail.com>
2016-06-14 14:40:20 -0500

    improve eigenClean

--- eigenClean.py
+++ eigenClean.py
@@ -27,62 +27,82 @@ warnings.filterwarnings("ignore")
 
 
 class EigenEngine:
+    available_predict_method = ("scale_area", "least_square")
+
     def __init__(self):
         self.image_shape = None
-        self.n = None
+        self.N = None
         self.S = None
         self.V = None
         self.av = None
         self.ROI = (None, None, None, None)
-        self.scale_factor = None
+        self.predict_method = None
+        self.n = None
+        # cache parameter
+        self.mask = None
+        self.predict_factor = None
 
-    def calculate_scale_factor(self):
-        # mask = np.zeros(shape=self.image_shape, dtype=np.bool)
-        x0_start, x0_end, x1_start, x1_end = self.ROI
-        # mask[x0_start:x0_end, x1_start:x1_end] = True
-        # mask = mask.flatten()
-        # self.scale_factor = 1.0/(1-np.array([np.sum(vec[mask]**2) for vec in self.V]))
-        # self.scale_factor = 1.0/(1-np.array([linalg.norm(vec[mask]) for vec in self.V]))
-        A = self.image_shape[0] * self.image_shape[1]
-        self.scale_factor = A / (A - float((x0_end - x0_start) * (x1_end - x1_start)))
+    def remove_predict_factors(self):
+        self.mask = None
+        self.predict_factor = None
+
+    def calculate_predict_factors(self, x0_start, x0_end, x1_start, x1_end, method="least_square", n=None):
+        if self.ROI == (x0_start, x0_end, x1_start, x1_end) and self.predict_method == method and n == n:
+            if self.predict_factor is not None:
+                return
+
+        self.ROI = (x0_start, x0_end, x1_start, x1_end)
+        self.n = n
+        self.predict_factor = None
+        self.predict_method = method
+        mask = np.zeros(shape=self.image_shape, dtype=np.bool)
+        mask[x0_start:x0_end, x1_start:x1_end] = True
+        self.mask = mask.flatten()
+
+        if method == "scale_area":
+            # self.scale_factor = 1.0/(1-np.array([np.sum(vec[mask]**2) for vec in self.V]))
+            # self.scale_factor = 1.0/(1-np.array([linalg.norm(vec[mask]) for vec in self.V]))
+            A = self.image_shape[0] * self.image_shape[1]
+            self.predict_factor = A / (A - float((x0_end - x0_start) * (x1_end - x1_start)))
+        elif method == "least_square":
+            V_cp = np.copy(self.V[:n])
+            V_cp[:, self.mask] = 0
+            self.predict_factor = linalg.pinv(V_cp)
+        else:
+            raise ValueError("Method not found!! Avalialbe method:" + str(self.available_predict_method))
 
     def fit(self, data):
         self.image_shape = data[0].shape
-        self.n = len(data)
-        batch = np.zeros((self.n, self.image_shape[0] * self.image_shape[1]))
-        for i in range(self.n):
+        self.N = len(data)
+        batch = np.zeros((self.N, self.image_shape[0] * self.image_shape[1]))
+        for i in range(self.N):
             batch[i] = data[i].flatten()
         self.av = np.average(batch, axis=0)
         batch = batch - self.av
         U, self.S, self.V = linalg.svd(batch, full_matrices=0)
-        self.scale_factor = None
+        self.predict_factor = None
 
-    def predict_coeff(self, img, x0_start, x0_end, x1_start, x1_end, n=None):
+    def predict_coeff(self, img, x0_start, x0_end, x1_start, x1_end, method="least_square", n=None):
 
-        if self.ROI != (x0_start, x0_end, x1_start, x1_end):
-            self.ROI = (x0_start, x0_end, x1_start, x1_end)
-            self.scale_factor = None
+        self.calculate_predict_factors(x0_start, x0_end, x1_start, x1_end, method=method, n=n)
 
         if self.image_shape != img.shape:
-            pass
-        n0, n1 = img.shape
-        img_cp = img.copy()
-        img_cp -= self.av.reshape(self.image_shape)
-        img_cp[x0_start:x0_end, x1_start:x1_end] = 0
-        img_cp = img_cp.flatten()
+            raise ValueError("Image size is not correct!!")
 
-        if self.scale_factor is None:
-            self.calculate_scale_factor()
+        img_cp = img.copy().flatten()
+        img_cp -= self.av
+        img_cp[self.mask] = 0
 
-        c = np.dot(self.V[:n], img_cp.T)
-        if isinstance(self.scale_factor, np.ndarray):
-            c *= self.scale_factor[:n]
+        if method == "scale_area":
+            c = np.dot(self.V[:n], img_cp.T) * self.predict_factor
+        elif method == "least_square":
+            c = np.dot(img_cp, self.predict_factor)
         else:
-            c *= self.scale_factor
+            assert False
         return c
 
-    def predict(self, img, x0_start, x0_end, x1_start, x1_end, n=None):
-        c = self.predict_coeff(img, x0_start, x0_end, x1_start, x1_end, n)
+    def predict(self, img, x0_start, x0_end, x1_start, x1_end, method="least_square", n=None):
+        c = self.predict_coeff(img, x0_start, x0_end, x1_start, x1_end, method=method, n=n)
         return (np.dot(c, self.V[:n]) + self.av).reshape(self.image_shape)
 
     def save(self, fname):
@@ -94,9 +114,10 @@ class EigenEngine:
             tmp_dict = cPickle.load(f)
         self.__dict__.update(tmp_dict)
 
-    def remember_only(self, n):
-        self.V = self.V[n]
-        self.S = self.S[n]
+    def remember_only(self, N):
+        self.N = N
+        self.V = self.V[:N]
+        self.S = self.S[:N]
 
 
 class EigenClean:
@@ -155,7 +176,7 @@ class EigenClean:
             plt.savefig("report_learn.png")
             plt.close()
 
-    def predict(self, shots, roi, basis="basis", plot=False, verbose=True, n=None):
+    def predict(self, shots, roi, basis="basis", plot=False, verbose=True, predict_method="least_square", n=None):
         if verbose:
             print "Running backup"
         self.backup(shots)
@@ -167,6 +188,7 @@ class EigenClean:
         if self.EE.av is None:  # No basis are loaded.
             self.EE.load(basis)
 
+        print "Prediction Method: {0}".format(predict_method)
         for shot in shots:
             if verbose:
                 print "Predicting #{0}".format(shot)
@@ -178,7 +200,7 @@ class EigenClean:
                     img_atom = p_atom[0].data[0]
 
                     # print roi_converted
-                    bg_predict = self.EE.predict(img_atom, *roi_converted, n=n)
+                    bg_predict = self.EE.predict(img_atom, *roi_converted, n=n, method=predict_method)
                     bg_predict = bg_predict.astype(img_atom.dtype)
                     p_noatom[0].data[0] = bg_predict
                     # p_noatom.flush()
@@ -191,8 +213,8 @@ class EigenClean:
                         y = np.arange(0, img_atom.shape[0])
                         od_original = np.divide(img_atom, bg_original, dtype=np.float)
                         od_new = np.divide(img_atom, bg_predict, dtype=np.float)
-                        od_original_cp = od_original.copy()
-                        od_new_cp = od_new.copy()
+                        od_original_cp = np.copy(od_original)
+                        od_new_cp = np.copy(od_new)
                         od_original_cp[roi_converted[0]:roi_converted[1], roi_converted[2]:roi_converted[3]] = 1
                         od_new_cp[roi_converted[0]:roi_converted[1], roi_converted[2]:roi_converted[3]] = 1
 
@@ -207,21 +229,23 @@ class EigenClean:
                         plt.ylim(0, img_atom.shape[0])
 
                         plt.subplot("222")
-                        plt.title("Optimized")
+                        plt.title("Optimized, Method: {0}".format(predict_method))
                         plt.pcolormesh(x, y, od_new)
                         plt.xlim(0, img_atom.shape[1])
                         plt.ylim(0, img_atom.shape[0])
                         plt.colorbar()
 
                         plt.subplot("223")
-                        plt.title("Original BG")
+                        error = np.sum((od_original_cp-1) ** 2)
+                        plt.title("Original BG, error = {0:.2f}".format(error))
                         plt.pcolormesh(od_original_cp)
                         plt.colorbar()
                         plt.xlim(0, img_atom.shape[1])
                         plt.ylim(0, img_atom.shape[0])
 
                         plt.subplot("224")
-                        plt.title("Optimized BG")
+                        error = np.sum((od_new_cp-1)**2)
+                        plt.title("Optimized BG, error = {0:.2f}".format(error))
                         plt.pcolormesh(x, y, od_new_cp)
 
                         plt.xlim(0, img_atom.shape[1])
@@ -250,10 +274,13 @@ if __name__ == '__main__':
     parser.add_argument('--predict', action='store_true')
     parser.add_argument('--no_plot', action='store_true')
     parser.add_argument('-n', action="store", dest='n', type=int, default=None)
+    parser.add_argument('--predict_method', action="store", dest='predict_method', type=str, default="least_square",
+                        help= "Avalialbe method:" + str(EigenEngine.available_predict_method))
     args = parser.parse_args()
     shots = qrange.parse_range(args.range)
     shots = [int(shot) for shot in shots]
     n = args.n
+    predict_method = args.predict_method
 
     if args.no_plot:
         plot = False
@@ -271,4 +298,4 @@ if __name__ == '__main__':
     elif args.predict:
         roi = args.roi.split(",")
         roi = [int(i) for i in roi]
-        EC.predict(shots, roi, plot=plot, n=n)
+        EC.predict(shots, roi, plot=plot, predict_method=predict_method ,n=n)
