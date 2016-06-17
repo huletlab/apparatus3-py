3319273ec8a9a8be753a92424cc2c9c528826647
gilbert <ldes89150@gmail.com>
2016-06-17 14:29:28 -0500

    update bragg1Danalysis

--- bragg1Danalysis.py
+++ bragg1Danalysis.py
@@ -9,10 +9,13 @@ import matplotlib
 matplotlib.use('Agg')
 
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
 import numpy as np
 from scipy.constants import *
 from scipy.optimize import curve_fit
 from configobj import ConfigObj
+from scipy import ndimage
 
 import qrange
 
@@ -58,21 +61,22 @@ except:
     pass
 
 
-def bragg_1D_anlysis(datadir, shot, shot_ref, report=None, verbose=True, save_fig=True, rotate=False):
+def bragg_1D_anlysis(datadir, shot, shot_ref, report=None, verbose=True, save_fig=True, rotate=False, roi=None,
+                     section='1DBRAGG_ANALYSIS'):
     shot_num = int(float(report['SEQ']['shot']))
 
     if not report:
         inifile = datadir + 'report' + "%04d" % shot + '.INI'
         report = ConfigObj(inifile)
 
-    if not '1DBRAGG_ANALYSIS' in report.keys():
-        report['1DBRAGG_ANALYSIS'] = {}
+    if section not in report.keys():
+        report[section] = {}
     try:
         ao0_freq = float(report['1DBRAGG']['AO0Freq'])
         ao1_freq = float(report['1DBRAGG']['AO1Freq'])
 
         delta_freq = ao0_freq - ao1_freq
-        report['1DBRAGG_ANALYSIS']['DeltaFreq'] = delta_freq
+        report[section]['DeltaFreq'] = delta_freq
 
     except Exception as e:
         if isinstance(shot, np.ndarray):
@@ -108,6 +112,10 @@ def bragg_1D_anlysis(datadir, shot, shot_ref, report=None, verbose=True, save_fi
 
     y = np.sum(diff, axis=0)
     x = np.arange(len(y))
+    # y_original = np.sum(cddata, axis=0)
+    # y_original = 1e4*np.exp(-(x-70.)**2/2.)
+    y_original = np.sum(cddata, axis=0)
+    x_original = np.arange(len(y_original))
     y_fit = double_gaussian(x, *parameters)
     y_fit_nobg = double_gaussian(x, a, mu1, mu2, sigma, 0)
 
@@ -126,14 +134,36 @@ def bragg_1D_anlysis(datadir, shot, shot_ref, report=None, verbose=True, save_fi
 
     com_p = np.average(x, weights=y_positive)
     com_n = np.average(x, weights=y_negative)
-
+    com_original = ndimage.measurements.center_of_mass(cddata)
     shift_distance = (com_p - com_n) * magnif
+    y_original_positive = np.copy(y_original)
+    y_original_positive[y_original_positive < 500] = 0
+    com_original_positive = np.average(x_original, weights=y_original_positive)
 
-    report['1DBRAGG_ANALYSIS']['NumScattering'] = num_scattering
-    report['1DBRAGG_ANALYSIS']['ShiftDistance'] = shift_distance  # store in um
-    report['1DBRAGG_ANALYSIS']['Amplitude'] = abs(a)
-    report['1DBRAGG_ANALYSIS']['contrast'] = contrast
-    report['1DBRAGG_ANALYSIS']['MomentumTransfer'] = num_scattering * shift_distance
+    if roi is not None:
+        s = 0.0
+        for i in range(cddata.shape[0]):
+            for j in range(cddata.shape[1]):
+                if i > roi[0] and i < roi[1]:
+                    if j > roi[2] and j < roi[3]:
+                        s += cddata[i, j]
+
+    peakSep = (np.argmax(y_fit) - np.argmin(y_fit)) * magnif
+
+    report[section]['NumScattering'] = num_scattering
+    report[section]['ShiftDistance'] = shift_distance  # store in um
+    report[section]['peakSep'] = peakSep  # store in um
+    report[section]['Amplitude'] = abs(a)
+    report[section]['contrast'] = contrast
+    report[section]['MomentumTransfer'] = num_scattering * shift_distance
+    report[section]['OriginalCOMx'] = com_original[1]
+    report[section]['OriginalCOMx_positive'] = com_original_positive
+    report[section]['OriginalCOMy'] = com_original[0]
+    # report[section]['1dmax'] = np.max(y_original)
+    # report[section]['1dmin'] = np.min(y_original)
+
+    if roi is not None:
+        report[section]['partial_sum'] = s
 
     if verbose:
         if isinstance(shot, np.ndarray):
@@ -148,19 +178,40 @@ def bragg_1D_anlysis(datadir, shot, shot_ref, report=None, verbose=True, save_fi
     report.write()
 
     if save_fig:
-        plt.figure(figsize=(5, 10), dpi=80)
-        plt.subplot('211')
+        plt.figure(figsize=(5, 15), dpi=80)
+        plt.subplot('311')
+        plt.imshow(cddata)
+        plt.plot([com_original[1]], [com_original[0]], 'wo')
+        if roi is not None:
+            currentAxis = plt.gca()
+            someX = (roi[2] + roi[3]) / 2.0
+            someY = (roi[1] + roi[0]) / 2.0
+            currentAxis.add_patch(patches.Rectangle((someX - .5, someY - .5), roi[3] - roi[2], roi[1] - roi[0],
+                                                    facecolor="grey", fill=False))
+            # ref: http://stackoverflow.com/questions/13013781/how-to-draw-a-rectangle-over-a-specific-region-in-a-matplotlib-graph
+
+        plt.subplot('312')
         if isinstance(shot, np.ndarray):
             plt.title("#{0}".format(shot_num) + r"$\Delta$ = {0}kHz".format(delta_freq * 1000))
         else:
             plt.title("#{0} - #{1}".format(shot, shot_ref) + r"$\Delta$ = {0}kHz".format(delta_freq * 1000))
         plt.imshow(diff)
+
         plt.colorbar()
-        plt.subplot('212')
-        plt.plot(x, y)
+
+        plt.subplot('313')
+        plt.plot(x, y, label="Diff")
+        plt.plot(x_original, y_original, label="Original")
         plt.plot(x, y_fit,
-                 label="NumScattering = {0:.0f}\nShiftDistance = {1:.2f} um".format(num_scattering, shift_distance))
-        plt.ylim(y.min() * 1.3, y.max() * 1.3)
+                 label="NumScattering = {0:.0f}\nShiftDistance = {1:.2f} um\npeakSep={2:.2f}um".format(num_scattering,
+                                                                                                       shift_distance,
+                                                                                                       peakSep))
+        plt.axvline(com_original[1], color='deeppink', label="COM_2D")
+        y_original_positive = np.copy(y_original)
+        y_original_positive[y_original_positive < 0] = 0
+        plt.axvline(np.average(x_original, weights=y_original_positive), color='g', label="COM_1D_pos")
+        plt.legend()
+        plt.ylim(y.min() * 1.3, y_original.max() * 1.3)
         plt.legend(loc='best').get_frame().set_alpha(0.5)
 
         plt.savefig('%04d_bragg_1D_analysis.png' % shot_num)
@@ -172,16 +223,31 @@ def bragg_multi(datadir, shots, **kwargs):
     cddatas = [np.loadtxt(os.path.join(datadir, '%04d_column.ascii' % shot)) for shot in shots]
     cddata_refs = []
     print "Searching for ref shots:"
+    cddata_dict = {}
     for i, report in enumerate(reports):
+        delta = float(report['1DBRAGG']['AO0Freq']) - float(report['1DBRAGG']['AO1Freq'])
+        if not delta in cddata_dict:
+            cddata_dict[delta] = []
+        cddata_dict[delta].append(cddatas[i])
         if float(report['1DBRAGG']['AO0Freq']) == float(report['1DBRAGG']['AO1Freq']):
             print "#{0}".format(shots[i])
             cddata_refs.append(cddatas[i])
     cddata_ref = np.mean(cddata_refs, axis=0)
+    cddata_mean_dict = {}
+    for key, cddata_list in cddata_dict.iteritems():
+        cddata_mean_dict[key] = np.mean(cddata_list, axis=0)
 
     print "Analysis begin:"
     for i, report in enumerate(reports):
         try:
-            bragg_1D_anlysis(datadir, cddatas[i], cddata_ref, report=report, **kwargs)
+            bragg_1D_anlysis(datadir, cddata, cddata_ref, report=report, **kwargs)
+        except Exception as err:
+            print err
+    for i, report in enumerate(reports):
+        delta = float(report['1DBRAGG']['AO0Freq']) - float(report['1DBRAGG']['AO1Freq'])
+        try:
+            bragg_1D_anlysis(datadir, cddata_mean_dict[delta], cddata_ref, report=report, section='1DBRAGG_ANALYSIS',
+                             **kwargs)
         except Exception as err:
             print err
 
@@ -192,16 +258,22 @@ if __name__ == '__main__':
     parser.add_argument('--shot', type=int)
     parser.add_argument('--ref', type=int)
     parser.add_argument('--no_save_fig', action='store_false')
+    parser.add_argument('--ROI', action="store", dest='roi', help="")
+
     args = parser.parse_args()
     shot = args.shot
     shot_ref = args.ref
     save_fig = args.no_save_fig
     datadir = "./"
+    roi = args.roi
+    if roi is not None:
+        roi = roi.spl.split(",")
+        roi = [int(i) for i in roi]
 
     if args.range:
         shots = qrange.parse_range(args.range)
         shots = [int(shot) for shot in shots]
-        bragg_multi(datadir, shots, save_fig=save_fig, verbose=True)
+        bragg_multi(datadir, shots, save_fig=save_fig, verbose=True, roi=roi)
 
     else:
-        bragg_1D_anlysis(datadir, shot, shot_ref, save_fig=save_fig, verbose=True)
+        bragg_1D_anlysis(datadir, shot, shot_ref, save_fig=save_fig, verbose=True, roi=roi)
